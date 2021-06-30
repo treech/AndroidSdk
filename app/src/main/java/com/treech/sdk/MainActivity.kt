f7d9db71d80a0a597ce11596ba9f604c5b93f12b
ygq <yeguoqiang6@outlook.com>
2021-06-30 19:42:07 +0800

    添加可拖拽悬浮按钮控件

--- app/src/main/java/com/treech/sdk/MainActivity.kt
+++ app/src/main/java/com/treech/sdk/MainActivity.kt
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
