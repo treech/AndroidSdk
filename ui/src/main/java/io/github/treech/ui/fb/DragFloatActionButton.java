8253b65720225e70c7a26d158fd2f706890db6d2
ygq <yeguoqiang6@outlook.com>
2021-08-10 14:19:31 +0800

    带动画的checkbox

---
+++ ui/src/main/java/io/github/treech/ui/fb/DragFloatActionButton.java
@@ -0,0 +1,129 @@
+package io.github.treech.ui.fb;
+
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
+
+import androidx.core.view.ViewConfigurationCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+/**
+ * 可拖拽悬浮按钮，有吸附效果
+ */
+public class DragFloatActionButton extends FloatingActionButton {
+
+    private int parentHeight;
+    private int parentWidth;
+    private int slop;
+
+    public DragFloatActionButton(Context context) {
+        this(context, null);
+    }
+
+    public DragFloatActionButton(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+
+    }
+
+    public DragFloatActionButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        slop = ViewConfigurationCompat.getScaledPagingTouchSlop(ViewConfiguration.get(context));
+    }
+
+
+    private int lastX;
+    private int lastY;
+    private boolean isDrag;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int rawX = (int) event.getRawX();
+        int rawY = (int) event.getRawY();
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                setPressed(true);
+                isDrag = false;
+                getParent().requestDisallowInterceptTouchEvent(true);
+                lastX = rawX;
+                lastY = rawY;
+                ViewGroup parent;
+                if (getParent() != null) {
+                    parent = (ViewGroup) getParent();
+                    parentHeight = parent.getHeight();
+                    parentWidth = parent.getWidth();
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (parentHeight <= 0 || parentWidth <= 0) {
+                    //如果不存在父类的宽高则无法拖动，默认直接返回false
+                    isDrag = false;
+                    break;
+                }
+                int dx = rawX - lastX;
+                int dy = rawY - lastY;
+                //这里修复一些华为手机无法触发点击事件
+                int distance = (int) Math.sqrt(dx * dx + dy * dy);
+                //此处稍微增加一些移动的偏移量，防止手指抖动，误判为移动无法触发点击时间
+                if (distance == 0 || distance <= slop) {
+                    isDrag = false;
+                    break;
+                }
+                //程序到达此处一定是正在拖动了
+                isDrag = true;
+                float x = getX() + dx;
+                float y = getY() + dy;
+                //检测是否到达边缘 左上右下
+                x = x < 0 ? 0 : x > parentWidth - getWidth() ? parentWidth - getWidth() : x;
+                y = getY() < 0 ? 0 : getY() + getHeight() > parentHeight ? parentHeight - getHeight() : y;
+                setX(x);
+                setY(y);
+                lastX = rawX;
+                lastY = rawY;
+                break;
+            case MotionEvent.ACTION_UP:
+                if (isDrag()) {
+                    //恢复按压效果
+                    setPressed(false);
+                }
+                welt(rawX);
+                break;
+        }
+        //如果是拖拽则消耗事件，否则正常传递即可。
+        return isDrag() || super.onTouchEvent(event);
+    }
+
+    private boolean isDrag() {
+        return isDrag;
+    }
+
+    private boolean isLeftSide() {
+        return getX() == 0;
+    }
+
+    private boolean isRightSide() {
+        return getX() == parentWidth - getWidth();
+    }
+
+    private void welt(int currentX) {
+        if (!isLeftSide() || !isRightSide()) {
+            if (currentX >= parentWidth / 2) {
+                //靠右吸附
+                animate().setInterpolator(new DecelerateInterpolator())
+                        .setDuration(500)
+                        .xBy(parentWidth - getWidth() - getX())
+                        .start();
+            } else {
+                //靠左吸附
+                ObjectAnimator oa = ObjectAnimator.ofFloat(this, "x", getX(), 0);
+                oa.setInterpolator(new DecelerateInterpolator());
+                oa.setDuration(500);
+                oa.start();
+            }
+        }
+    }
+}
